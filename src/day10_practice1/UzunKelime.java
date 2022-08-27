bec94e8a7d98a67cc77ead82d565f60380e9a9f4
Emrullah Celebi <cumali20182019@gmail.com>
2022-08-27 10:11:29 +0200

    2708221011

---
+++ src/day10_practice1/UzunKelime.java
@@ -0,0 +1,2 @@
+package day10_practice1;public class a {
+}
---
+++ src/day10_practice1/okul_projesi/Giris.java
@@ -0,0 +1,42 @@
+package day10_Practice.okul_proje;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Giris {
+
+ /*
+     * ogretmen bilgilerini kullanicidan aliniz
+     * aldiginiz bilgilerrden object ureterek yeni
+      ogretmenler olusturun ve bilgilerini yazdirin
+     */
+
+    static Scanner scan=new Scanner(System.in);
+    static List<OgretmenBilgileri> ogretmenList=new ArrayList<>();
+ public static void main(String[] args) {
+     System.out.println("Hos geldiniz okulumuza ");
+
+     System.out.println("isim giriniz");
+
+     String isim=scan.nextLine();
+
+     System.out.println("soyisim giriniz");
+     String soyisim=scan.nextLine();
+     System.out.println("yasinizi giriniz");
+     int yas=scan.nextInt();
+     scan.nextLine();//hayalet DUMMY
+     System.out.println("bransinizi giriniz");
+     String brans=scan.nextLine();
+
+     System.out.println("tel no giriniz");
+     String tel=scan.nextLine();
+
+     OgretmenBilgileri adayOgretmen=new OgretmenBilgileri(isim,soyisim,yas, brans,tel);
+
+
+     ogretmenList.add(adayOgretmen);
+
+     System.out.println("ogretmenList = " + ogretmenList);
+ }
+}
